4d6bc418e28426001b6612e43b644a2e734d9826
GioDev02 <giovannidce@gmail.com>
2025-11-18 18:18:29 -0500

    feat(*): new change in book

--- src/pages/[book]/index.js
+++ src/pages/[book]/index.js
@@ -1,1 +1,41 @@
-export { default } from "./book";
+import React from 'react'
+import { BasicLayout } from "@/layouts";
+import { Book } from "@/components/Book";
+import { Separator, Seo } from "@/components/Shared";
+export default function BookPage(props) {
+    const { book } = props;
+    const wallpaper = book.attributes.wallpaper;
+    return (
+        <>
+            <Seo
+                title={book.attributes.title}
+                description={book.attributes.summary}
+            />
+
+            <BasicLayout>
+                <Book.HeaderWallpaper image={wallpaper.data.attributes.url} />
+                <Book.Panel bookId={book.id} book={book.attributes} />
+                <Separator height={50} />
+                <Book.Info book={book.attributes} />
+                <Separator height={30} />
+                <Book.Media video={book.attributes.video}
+                    screenshots={book.attributes.screenshots.data}
+                />
+            </BasicLayout>
+        </>
+    );
+}
+
+export async function getServerSideProps(context) {
+    const {
+        params: { book },
+    } = context;
+    const bookCtrl = new Book();
+    const response = await bookCtrl.getBySlug(book);
+    console.log("dinamica", response);
+    return {
+        props: {
+            book: response,
+        },
+    };
+}
